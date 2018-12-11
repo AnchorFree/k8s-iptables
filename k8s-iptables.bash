5e4ec70dc2968ddd2ebda4c3eb981be7ecc5b4b5
Vladimir Zorin <vladimir@deviant.guru>
2018-12-11 19:52:05 +0200

    Add default drop policy and infinite apply loop

--- k8s-iptables.bash
+++ k8s-iptables.bash
@@ -3,8 +3,9 @@
 POLICY_FILE="${POLICY_FILE:-DEFAULT}"
 POLICY_REVISION="${POLICY_REVISION:-k8s-iptables-default-rev1}"
 CHECK_INTERVAL="${CHECK_INTERVAL:-60}"
+DEFAULT_POLICY_MODE="${DEFAULT_POLICY_MODE:-DROP}"
 DEFAULT_POLICY_COMMENT="${DEFAULT_POLICY_COMMENT:-k8s-iptables-default-policy}"
-MAX_TRIES="${MAX_TRIES:-15}"
+MAX_TRIES="${MAX_TRIES:-0}"
 
 failedAttempts=0
 
@@ -13,7 +14,10 @@ applyDefaultPolicy () {
     policyComment="${DEFAULT_POLICY_COMMENT}"
     if [[ ! -f "${POLICY_FILE}" ]]; then
         policyComment="${POLICY_REVISION}"
-    fi
+   fi
+
+    if [[ "${DEFAULT_POLICY_MODE}" == "DROP" ]]; then
+
     cat <<EOF | iptables-restore -T raw
 *raw
 :PREROUTING ACCEPT [0:0]
@@ -22,7 +26,27 @@ applyDefaultPolicy () {
 COMMIT
 EOF
 
-    echo "$(date +%s): applied default policy"
+    else 
+
+    cat <<EOF | iptables-restore -T raw
+*raw
+:PREROUTING DROP [0:0]
+:OUTPUT ACCEPT [0:0]
+-A PREROUTING -m comment --comment "${policyComment}"
+-A PREROUTING -p tcp -s 127.0.0.0/8 -j ACCEPT
+-A PREROUTING -p tcp -d 127.0.0.0/8 -j ACCEPT
+-A PREROUTING -p tcp -s 10.0.0.0/8 -j ACCEPT
+-A PREROUTING -p tcp -d 10.0.0.0/8 -j ACCEPT
+-A PREROUTING -p tcp -s 172.16.0.0/12 -j ACCEPT
+-A PREROUTING -p tcp -d 172.16.0.0/12 -j ACCEPT
+-A PREROUTING -p tcp -s 192.168.0.0/16 -j ACCEPT
+-A PREROUTING -p tcp -d 192.168.0.0/16 -j ACCEPT
+-A PREROUTING -p tcp --dport 22 -j ACCEPT
+EOF
+
+    fi
+
+    echo "$(date +%s): applied default ${DEFAULT_POLICY_MODE} policy"
 }
 
 applyPolicy () {
@@ -59,8 +83,12 @@ while :; do
     
     sleep ${CHECK_INTERVAL}
     if ! policyExists; then 
-        if [[ ${failedAttempts} < ${MAX_TRIES} ]]; then
+        if [[ ${MAX_TRIES} == 0 ]]; then
             applyPolicy
+        else
+            if [[ ${failedAttempts} < ${MAX_TRIES} ]]; then
+                applyPolicy
+            fi
         fi
     fi
 
